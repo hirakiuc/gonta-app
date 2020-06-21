3c6b442de92ec7c340524307dfa186a8f0824ac8
hirakiuc <hirakiuc@mercari.com>
2020-06-21 14:18:57 +0900

    refactored(not works yet)

--- handler/beer_select_handler.go
+++ handler/beer_select_handler.go
@@ -2,17 +2,18 @@ package handler
 
 import (
 	"context"
+	"fmt"
 
+	"github.com/slack-go/slack"
 	"github.com/slack-go/slack/slackevents"
+	"go.uber.org/zap"
 )
 
-/*
 const (
 	actionSelect = "select"
-	actionStart  = "start"
-	actionCancel = "cancel"
+	// actionStart  = "start"
+	// actionCancel = "cancel"
 )
-*/
 
 // BeerSelectHandler describe a instance of BeerSelect Request.
 type BeerSelectHandler struct {
@@ -24,64 +25,77 @@ func NewBeerSelectHandler() *BeerSelectHandler {
 	return &BeerSelectHandler{}
 }
 
+func (h *BeerSelectHandler) innerEvent(e *slackevents.EventsAPIEvent) (*slackevents.AppMentionEvent, error) {
+	innerEvent := e.InnerEvent
+
+	switch ev := innerEvent.Data.(type) {
+	case *slackevents.AppMentionEvent:
+		return ev, nil
+	default:
+		err := fmt.Errorf("%w", ErrUnexpectedEventType)
+		h.log.Error("unexpected event type", zap.Error(err))
+
+		return nil, err
+	}
+}
+
 // Handler a beer select event.
-func (h *BeerSelectHandler) Handle(_ context.Context, event *slackevents.EventsAPIEvent) error {
+func (h *BeerSelectHandler) Handle(_ context.Context, e *slackevents.EventsAPIEvent) error {
 	log := h.log
 
 	log.Debug("BaseSelectReplyer reply:empty")
-	/*
-		client, err := GetClient()
-		if err != nil {
-			// Can't respond to the event.
-			w.WriteHeader(http.StatusOK)
-			return
-		}
 
-		attachment := slack.Attachment{
-			Text:       "Which beer do you want? :beer:",
-			Color:      "#f9a41b",
-			CallbackID: "beer",
-			Actions: []slack.AttachmentAction{
-				{
-					Name: actionSelect,
-					Type: "select",
-					Options: []slack.AttachmentActionOption{
-						{
-							Text:  "Asahi Super Dry",
-							Value: "Asahi Super Dry",
-						},
-						{
-							Text:  "Kirin Lager Beer",
-							Value: "Kirin Lager Beer",
-						},
-						{
-							Text:  "Sapporo Black Label",
-							Value: "Sapporo Black Label",
-						},
-						{
-							Text:  "Suntory Malts",
-							Value: "Suntory Malts",
-						},
-						{
-							Text:  "Yona Yona Ale",
-							Value: "Yona Yona Ale",
-						},
+	ev, err := h.innerEvent(e)
+	if err != nil {
+		return err
+	}
+
+	api := slackAPI()
+
+	attachment := slack.Attachment{
+		Text:       "Which beer do you want? :beer:",
+		Color:      "#f9a41b",
+		CallbackID: "beer",
+		Actions: []slack.AttachmentAction{
+			{
+				Name: actionSelect,
+				Type: "select",
+				Options: []slack.AttachmentActionOption{
+					{
+						Text:  "Asahi Super Dry",
+						Value: "Asahi Super Dry",
+					},
+					{
+						Text:  "Kirin Lager Beer",
+						Value: "Kirin Lager Beer",
+					},
+					{
+						Text:  "Sapporo Black Label",
+						Value: "Sapporo Black Label",
+					},
+					{
+						Text:  "Suntory Malts",
+						Value: "Suntory Malts",
+					},
+					{
+						Text:  "Yona Yona Ale",
+						Value: "Yona Yona Ale",
 					},
 				},
 			},
-		}
+		},
+	}
 
-			channelID, timestamp, err := client.PostMessage(
-				e.ChannelID,
-				slack.MsgOptionText("Some Text", false),
-				slack.MsgOptionAttachments(attachment),
-			)
-			if err != nil {
-				log.Error("BaseSelectReplyer failed", zap.Error(err))
-			}
+	channelID, tstamp, err := api.PostMessage(
+		ev.Channel,
+		slack.MsgOptionText("Some Text", false),
+		slack.MsgOptionAttachments(attachment),
+	)
+	if err != nil {
+		log.Error("BaseSelectReplyer failed", zap.Error(err))
+	}
 
-			log.Debug("Post Message", zap.String("channelID", channelID), zap.String("timestamp", timestamp))
-	*/
+	log.Debug("Post Message", zap.String("channelID", channelID), zap.String("timestamp", tstamp))
 
 	return nil
 }
