01d97a59d9acb57a6ef3d5f65c3c8a10f57c5baa
hirakiuc <hirakiuc@mercari.com>
2020-07-18 18:53:16 +0900

    Use zapdriver for cloud run

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/hirakiuc/gonta-app
 go 1.14
 
 require (
+	github.com/blendle/zapdriver v1.3.1
 	github.com/gorilla/websocket v1.4.0 // indirect
 	github.com/kelseyhightower/envconfig v1.4.0
 	github.com/pkg/errors v0.9.1 // indirect
--- log/logger.go
+++ log/logger.go
@@ -4,39 +4,21 @@ import (
 	"fmt"
 	"os"
 
+	"github.com/blendle/zapdriver"
 	"go.uber.org/zap"
-	"go.uber.org/zap/zapcore"
 )
 
 // GetLogger return a zap.Logger instance.
 func GetLogger() *zap.Logger {
-	level := zap.NewAtomicLevel()
-	level.SetLevel(zapcore.DebugLevel)
-
-	config := zap.Config{
-		Level:    level,
-		Encoding: "json",
-		EncoderConfig: zapcore.EncoderConfig{
-			TimeKey:        "Time",
-			LevelKey:       "Level",
-			NameKey:        "Name",
-			CallerKey:      "Caller",
-			MessageKey:     "Msg",
-			StacktraceKey:  "StackTrace",
-			EncodeLevel:    zapcore.CapitalLevelEncoder,
-			EncodeTime:     zapcore.ISO8601TimeEncoder,
-			EncodeDuration: zapcore.StringDurationEncoder,
-			EncodeCaller:   zapcore.ShortCallerEncoder,
-		},
-		OutputPaths:      []string{"stdout"},
-		ErrorOutputPaths: []string{"stderr"},
-	}
-
-	logger, err := config.Build()
+	logger, err := zapdriver.NewProductionWithCore(
+		zapdriver.WrapCore(
+			zapdriver.ReportAllErrors(true),
+			zapdriver.ServiceName("gonta"),
+		),
+	)
 	if err != nil {
-		// Fatal error
 		fmt.Fprintf(os.Stderr, "Failed to create logger.")
-		os.Exit(1)
+		return nil
 	}
 
 	return logger
